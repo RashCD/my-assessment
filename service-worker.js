f40b4a6d8b1de34e2376804118cf2dfba264b07b
Rashidi <mrrash91@gmail.com>
2019-02-26 17:08:02 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RashCD/my-assessment/precache-manifest.ce8a6c54eb4294adb06f6bb5376d3294.js"
+  "/my-assessment/precache-manifest.55e538d1ebe4e2c2498969cd4fbcf121.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RashCD/my-assessment/index.html", {
+workbox.routing.registerNavigationRoute("/my-assessment/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
